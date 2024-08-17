838656f81863822bf21d3400f52b63f8954203ea
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-08-17 16:57:35 +0000

    Added basic DOM interaction

--- script.js
+++ script.js
@@ -17,9 +17,28 @@ const addBookToLibrary = (title, author, pages, read) => {
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
 
 addBookToLibrary("test", "test2", 200, "not read yet");
+addBookToLibrary(
+  "The Hitchhiker's Guide to the Galaxy",
+  "Douglas Adams",
+  216,
+  "read"
+);
+
+let table = document.querySelector("#libraryTable");
 
 const displayBook = (library) => {
   for (const book of library) {
-    console.log(book.title);
+    let row = table.insertRow(-1);
+    let c0 = row.insertCell(0);
+    let c1 = row.insertCell(1);
+    let c2 = row.insertCell(2);
+    let c3 = row.insertCell(3);
+
+    c0.innerText = book.title;
+    c1.innerText = book.author;
+    c2.innerText = book.pages;
+    c3.innerText = book.read;
   }
 };
+
+displayBook(myLibrary);
