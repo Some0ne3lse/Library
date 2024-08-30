326a523c31f60a8e17fc2019473c7e480aa6d944
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-08-30 16:18:59 +0000

    Change constructor to class

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  };
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 const addBookToLibrary = (title, author, pages, read) => {
